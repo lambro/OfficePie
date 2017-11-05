0bc6e96dce3781cfe5f0b6d4123ee77696720606
Laura Ambrose <laura@kintohub.com>
2017-11-05 15:29:01 +0800

    first react-native project

---
+++ App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import App from './App';
+import HelloWorldApp from './App';
+
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('displays the text hello world', () => {
+  const component = renderer.create(<HelloWorldApp />).toJSON();
+  const expected = ["Hello world!"]
+  expect(component.children).toEqual(expected)
+  console.log(component.children)
+})
